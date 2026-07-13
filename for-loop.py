0c421d125791c6d1a24ee8dcabe3a07747bcaf2c
Denmark-DC-Bartolome <denmark.dc.bartolome@gmail.com>
2026-07-14 01:35:29 +0800

    sanitization and Impovement

--- for-loop.py
+++ for-loop.py
@@ -1,9 +1,16 @@
+separator = "-"
+
+while len(separator) <= 20:
+    separator += "-"
+
+
 # Iterate over a list
 students = ["Ronwald", "Michaella", "Lorein", "Denmark"]
 for student in students:
     print(student)
 
-print("================================")
+print(separator)
+
 # using range() to generate numbers
 for i in range(5):
     print(i)
@@ -11,28 +18,30 @@ for i in range(5):
 print("")
 for i in range(2, 9):
     print(i)
-print(" ")
+print("----")
+print("\nskips 2 numbers:\n")
 for i in range(2, 10, 2):
     print(i)
 
-print("====================================")
+print(separator)
 # Iterate over string
 for char in "Hello World":
     print(char)
 
-print("====================================")
+
+print(separator)
+
 # With indec using enumerate()
 for index, student in enumerate(students):
     print(f"{index}:{student}")
 
-print("===============================================")
-print(" ")
-print(" While LOOP")
-print("    Repeats while condition is TRUE ")
+print(separator)
+print("\n While LOOP")
+print("--Repeats while condition is TRUE \n")
 
-print(" Example")
+print(" Example:")
 
-print("Counting up")
+print("\nCounting up\n")
 
 num = 0
 
@@ -40,8 +49,7 @@ while num <= 10:
     print(num)
     num += 1
 
-print("User input validation")
-print(" ")
+print("\nUser input validation\n")
 age = int(input("Enter Your Age(must be positibe): "))
 while age <= 0:
     print("Invalid! Must Be Positive. ")
@@ -52,20 +60,17 @@ passcode = int(input("Enter Passcode(number only!: "))
 while passcode != 143:
     print("wrong passcode.")
     passcode = int(input("Try again: "))
-print("Welcome!")
+print("Welcome!\n")
 
-
-print("===============================================")
-print("Loop Control Statement")
-print("     break : Exits Loops immediately")
-print("     continue : Skips the rest of the current  iteration and go to the next ")
+print(separator)
+print("\nLoop Control Statement\n")
+print("break : Exits Loops immediately")
+print("continue : Skips the rest of the current  iteration and go to the next ")
 print(
-    "     else : runs when the loop finishes all iterations (not interrupted by - break -"
+    "else : runs when the loop finishes all iterations (not interrupted by - break -\n"
 )
-print(" ")
-print("Example")
-print(" ")
-print("break")
+print("Example\n")
+print("---break---\n")
 for i in range(10):
     if i == 5:
         break
